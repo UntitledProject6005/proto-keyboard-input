e29fc2c18ded4c4d622f8a24f8547ff59794bafb
Kyu <rocketword@gmail.com>
2023-05-08 20:05:57 -0500

    Initial commit: console keyboard input

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ProtoKeyboardInput;
+
+class Program
+{
+        static void Main(string[] args)
+        {
+                // https://stackoverflow.com/a/46939556/3875151
+                ConsoleKey key;
+                
+                // While ConsoleKey is not Escape, print it out if its a spacebar
+                do
+                {
+                        while (!Console.KeyAvailable)
+                        { }
+
+                        key = Console.ReadKey(true).Key;
+
+                        if (key == ConsoleKey.Spacebar)
+                        {
+                                Console.WriteLine("  !");
+                        }
+                        Console.WriteLine(key);
+
+                } while (key != ConsoleKey.Escape);
+
+        }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# proto-keyboard-input  
+
+For investigatingh how to handle Keyboard Input in C#    
+
+Currently just outputs whatever key is being pressed (while console is focused)  
+
+## TODO  
+
+- [ ] Listen to all keyboard input regardless of console focus status
